97099d35a767dec6e3b60f20241bfc4b075b3e53
Quy Le <lnquy.it@gmail.com>
2019-09-11 22:41:16 +0700

    [features/admin] Styling nav bar

--- src/frontend/src/views/admin/Base.vue
+++ src/frontend/src/views/admin/Base.vue
@@ -11,26 +11,29 @@
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" app clipped class="elevation-5">
-      <v-list dense class="grey lighten-4">
-        <template v-for="(item, i) in navItems">
-          <v-row v-if="item.heading" :key="i" align="center">
-            <v-col cols="6">
-              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
-            </v-col>
-            <v-col cols="6" class="text-right">
-              <v-btn small text class="grey--text">edit</v-btn>
-            </v-col>
-          </v-row>
-          <v-divider v-else-if="item.divider" :key="i" dark class="my-4 mx-3"></v-divider>
-          <v-list-item v-else :key="i" @click="navItemClicked(item.id)">
-            <v-list-item-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title class="grey--text">{{ item.text }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </template>
+      <v-list dense rounded class="grey lighten-4">
+        <v-list-item-group v-model="navItemSelected" color="primary">
+          <template v-for="(item, i) in navItems">
+            <v-row v-if="item.heading" :key="i" align="center">
+              <v-col cols="6">
+                <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+              </v-col>
+              <v-col cols="6" class="text-right">
+                <v-btn small text class="grey--text">edit</v-btn>
+              </v-col>
+            </v-row>
+            <v-divider v-else-if="item.divider" :key="i" dark class="mt-3 mb-2 mx-3"></v-divider>
+
+            <v-list-item v-else :key="i" @click="navItemClicked(item.id)" selected>
+              <v-list-item-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title class="grey--text">{{ item.text }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
 
@@ -53,18 +56,19 @@ export default {
     drawer: null,
     navItems: [
       { id: 0, icon: "mdi-view-dashboard-outline", text: "Dashboard" },
-      { divider: true },
-      { heading: "Users" },
-      { id: 1, icon: "mdi-account-card-details-outline", text: "Manage users" },
-      { divider: true },
-      { heading: "Books" },
-      { id: 2, icon: "mdi-notebook-outline", text: "Manage books" },
-      { icon: "mdi-book-play-outline", text: "Archived" },
-      { icon: "mdi-delete-variant", text: "Deleted" },
-      { divider: true },
-      { id: 3, icon: "mdi-settings-outline", text: "Settings" },
-      { id: 4, icon: "mdi-help", text: "Help" }
+      { id: 1, divider: true },
+      { id: 2, heading: "Users" },
+      { id: 3, icon: "mdi-account-card-details-outline", text: "Manage users" },
+      { id: 4, divider: true },
+      { id: 5, heading: "Books" },
+      { id: 6, id: 2, icon: "mdi-notebook-outline", text: "Manage books" },
+      { id: 7, icon: "mdi-book-play-outline", text: "Archived" },
+      { id: 8, icon: "mdi-delete-variant", text: "Deleted" },
+      { id: 9, divider: true },
+      { id: 10, icon: "mdi-settings-outline", text: "Settings" },
+      { id: 11, icon: "mdi-help", text: "Help" }
     ],
+    navItemSelected: 0,
     pageTitle: "Dashboard"
   }),
   methods: {
@@ -72,23 +76,23 @@ export default {
       switch (id) {
         case 0: // Dashboard
           this.$router.push({ path: "/admin" });
-          this.pageTitle = "Dashboard"
+          this.pageTitle = "Dashboard";
           break;
-        case 1: // User management
+        case 3: // User management
           this.$router.push({ path: "/admin/users" });
-          this.pageTitle = "Users"
+          this.pageTitle = "Users";
           break;
-        case 2: // Book management
+        case 6: // Book management
           this.$router.push({ path: "/admin/books" });
-          this.pageTitle = "Books"
+          this.pageTitle = "Books";
           break;
-        case 3: // Settings
+        case 10: // Settings
           this.$router.push({ path: "/admin/settings" });
-          this.pageTitle = "Settings"
+          this.pageTitle = "Settings";
           break;
-        case 4: // Help
+        case 11: // Help
           this.$router.push({ path: "/admin/help" });
-          this.pageTitle = "Help"
+          this.pageTitle = "Help";
           break;
       }
     }
